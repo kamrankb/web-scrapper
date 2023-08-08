317cf9f4c097e5ce2fe872f7bd83df1ce09029cc
kamrankb <thekamrankb@gmail.com>
2023-08-09 03:05:53 +0500

    Added command to scrape data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        Commands\scrapeMovies::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -16,6 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('scrape:movies')->dailyAt('00:00');
     }
 
     /**
