ba687743dc93c131cc416c1208a9fa10faddf1d2
kamrankb <thekamrankb@gmail.com>
2023-08-09 03:01:31 +0500

    Implemented interface methods

--- app/Repositories/Scraper/ScraperRepository.php
+++ app/Repositories/Scraper/ScraperRepository.php
@@ -3,36 +3,71 @@
 namespace App\Repositories\Scraper;
 
 use App\Repositories\Scraper\ScraperInterface;
+use Exception;
 use Illuminate\Http\Request;
 use Goutte\Client;
+use InvalidArgumentException;
 
 class ScraperRepository implements ScraperInterface {
   protected $list;
   protected $jsonData;
 
   public function fetchWeb($url) {
-    $client = new Client();
-    
-    $website = $client->request('GET', 'https://www.imdb.com/chart/top');
-    
-    //$this->list = $website->filter('ul.ipc-metadata-list > li');
-    $this->jsonData = $website->filter('#__NEXT_DATA__')->text();
+    try {
+      $client = new Client();
+      
+      $website = $client->request('GET', 'https://www.imdb.com/chart/top');
+      
+      return [
+        "data" => $this->jsonData = $website->filter('#__NEXT_DATA__')->text()
+      ];
+    } catch(\InvalidArgumentException $invalid) {
+      return [
+        "error" => $invalid->getCode(),
+        "message" => $invalid->getMessage()
+      ];
+    } catch(\Exception $ex) {
+      return [
+        "error" => $ex->getCode(),
+        "message" => $ex->getMessage()
+      ];
+    }
   }
 
-  public function getMovies($movies=10) {
-    $json = json_decode($this->jsonData);
-    dd($json->props->pageProps->pageData->chartTitles->edges);
-    // h3 class="ipc-title__text
-    $count = 1;
-    $companies = $this->list->each(function ($node) use (&$count, $movies) {
-      dump($node->filter('h3.ipc-title__text')->text());
-      
-      if($count==$movies) {
-        return false;
+  public function getMovies($movieMount=10) {
+    try {
+      $json = json_decode($this->jsonData);
+      $movies = $json->props->pageProps->pageData->chartTitles->edges;
+      $movieLists = array();
+
+      foreach($movies as $index => $movie) {
+        $node = $movie->node;
+        $movieNode['title'] = $node->titleText->text;
+        $movieNode['year'] = $node->releaseYear->year;
+        $movieNode['rating'] = $node->ratingsSummary->aggregateRating;
+        $movieNode['movie_id'] = $node->id;
+
+        $movieNode['url'] = 'https://www.imdb.com/title/'.$movieNode['movie_id'];
+        
+        $movieLists[] = $movieNode;
+
+        if($movieMount == ($index+1)) {
+          break;
+        }
+
       }
-      
-      $count++;
-    });
-    //dd($this->list);
+
+      return $movieLists;
+    } catch(InvalidArgumentException $invalid) {
+      return [
+        "error" => $invalid->getCode(),
+        "message" => $invalid->getMessage()
+      ];
+    } catch(Exception $ex) {
+      return [
+        "error" => $ex->getCode(),
+        "message" => $ex->getMessage()
+      ];
+    }
   }
 }
